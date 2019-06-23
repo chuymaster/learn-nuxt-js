e64c084ae40f6c7f02704fde6ec58122ebe78cd2
Chuy <chuymaster3@hotmail.com>
2019-06-23 14:20:25 +0900

    Add post index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/users">Users</nuxt-link>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <nuxt-link :to="'/posts/' + 1" class="post-preview">
+        <article>
+          <div class="post-thumbnail" style="background-image: url('https://cdn.wccftech.com/wp-content/uploads/2018/10/Intel-9th-Gen-Core-2-Custom-2060x1317.jpg')"></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+       <nuxt-link :to="'/posts/' + 2" class="post-preview">
+        <article>
+          <div class="post-thumbnail" style="background-image: url('https://cdn.wccftech.com/wp-content/uploads/2018/10/Intel-9th-Gen-Core-2-Custom-2060x1317.jpg')"></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+    </section>
   </div>
 </template>
 
@@ -16,25 +38,78 @@ export default {
 
 <style>
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
 }
 
-.links {
-  padding-top: 15px;
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
 }
 </style>
