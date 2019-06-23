ee32ce607112f931d91fa2fc4c983d8e2dda84e5
Chuy <chuymaster3@hotmail.com>
2019-06-23 11:02:14 +0900

    First commit with users routing

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>A single user, with ID: {{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.id) // Check if id param is number
+    }
+}
+</script>
+
+<style>
+
+</style>
